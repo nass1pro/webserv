96c57feaf161ebb25b80584cf3269977536dba62
nass1pro <nass1pro@gmail.com>
2021-04-16 12:58:47 +0200

    add struct

--- main.cpp
+++ main.cpp
@@ -3,6 +3,19 @@
 #include <vector>
 #include <sys/socket.h>
 
+// #define PORT 8080
+// void setup_server()
+// {
+//     if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+//     {
+//         std::cout << "(cannot create socket)";
+//         exit(1);
+//     }
+//     address.sin_family = AF_INET;
+//     address.sin_addr.s_addr = INADDR_ANY;
+//     address.sin_port = htons( PORT );
+//     exit(1);
+// }
 
 
 int main(int ac, char **av)
@@ -12,9 +25,13 @@ int main(int ac, char **av)
         std::cout << "ERROR : file config needed"<< std::endl;
         exit(1);
     }
-    // 1 : parsing de la config
-    // 2 : setup server
+
+    // parsing de la config
+    // setupe server
+    // lancement du server
+        // attente de connection
 
 
+    setup_server();
     return 0;
 }
