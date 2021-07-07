a8bf253987aa940c3ef268f245c0820eb0b7553f
Elias Hafidi <ehafidi@fu-r7-p1.s19.be>
2021-07-07 12:27:15 +0200

    wefqwef

--- srcs/response/f_respons.cpp
+++ srcs/response/f_respons.cpp
@@ -6,7 +6,7 @@
 /*   By: ehafidi <ehafidi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/27 14:02:05 by nahaddac          #+#    #+#             */
-/*   Updated: 2021/07/07 12:25:28 by ehafidi          ###   ########.fr       */
+/*   Updated: 2021/07/07 12:27:00 by ehafidi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -357,7 +357,7 @@ void function_where_i_receive_request_data_and_return_response( std::map<int, t_
     t_res res;
 
 	std::cout << " \n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~" << std::endl;
-	std::cout << " \n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ METHOD : [" << req.method << "]" << std::endl;
+	// std::cout << " \n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ METHOD : [" << req.method << "]" << std::endl;
 	// std::cout << " \n~~~~ FULL REQUEST : [" << req.full_req << "] END OF FULL REQ" << std::endl;
 
 
