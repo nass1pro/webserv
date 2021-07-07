e0f27ab61f2a422a2717c129a82f982feb53646f
Elias Hafidi <ehafidi@fu-r7-p1.s19.be>
2021-07-07 14:09:13 +0200

    exit

--- srcs/response/f_respons.cpp
+++ srcs/response/f_respons.cpp
@@ -6,7 +6,7 @@
 /*   By: ehafidi <ehafidi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/27 14:02:05 by nahaddac          #+#    #+#             */
-/*   Updated: 2021/07/07 14:06:02 by ehafidi          ###   ########.fr       */
+/*   Updated: 2021/07/07 14:08:46 by ehafidi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -446,8 +446,13 @@ void function_where_i_receive_request_data_and_return_response( std::map<int, t_
             static int pass = 0;
 			pass++;
 			std::cout << "***************PASS INSIDE PUT [ " << pass << "]**********" << std::endl;
+			
 			request_put(res, config, req);
+
+			std::cout << "***************PASS out PUT [ " << pass << "]**********" << std::endl;
             concatenate_header(res, req);
+			exit(1);
+
             config.serv.res[client->first].append(res.response_header);
 			config.serv.res[client->first].append(res.payload);
         }
